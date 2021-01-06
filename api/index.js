db4d3ea75dd4d74c98f442e00eb6e6f2d1247642
yulei <1442268726@qq.com>
2021-01-06 18:31:59 +0800

    测试二下

--- api/index.js
+++ api/index.js
@@ -1,20 +1,20 @@
 module.exports = (req, res) => {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=计算', true);
-    xhr.onload = function (e) {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                console.log(xhr.responseText);
-            } else {
-                console.error(xhr.statusText);
-            }
-        }
-    };
-    xhr.onerror = function (e) {
-        console.error(xhr.statusText);
-    };
-    xhr.send(null);
-   
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=计算', true);
+//     xhr.onload = function (e) {
+//         if (xhr.readyState === 4) {
+//             if (xhr.status === 200) {
+//                 console.log(xhr.responseText);
+//             } else {
+//                 console.error(xhr.statusText);
+//             }
+//         }
+//     };
+//     xhr.onerror = function (e) {
+//         console.error(xhr.statusText);
+//     };
+//     xhr.send(null);
+
     const { name = 'World' } = req.query;
     res.send(`Hello ${name}!`);
 };
