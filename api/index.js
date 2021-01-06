f8b1323f5e5517b870ecf21110823ec030f395cd
yulei <1442268726@qq.com>
2021-01-06 18:15:32 +0800

    测试一下

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 module.exports = (req, res) => {
+    xmlhttp.open('GET', 'http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=计算', false);
+    xmlhttp.send();
+    console.log(xmlhttp.responseText;)
     const { name = 'World' } = req.query;
     res.send(`Hello ${name}!`);
 };
