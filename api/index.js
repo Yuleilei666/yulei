7efa0eefc417e612cb55ef017589ed90d1002981
yulei <1442268726@qq.com>
2021-01-06 18:27:46 +0800

    测试一下

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 module.exports = (req, res) => {
-    xmlhttp.open('GET', 'http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=计算', false);
-    xmlhttp.send();
-    console.log(xmlhttp.responseText)
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=计算', true);
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText);
+            } else {
+                console.error(xhr.statusText);
+            }
+        }
+    };
+    xhr.onerror = function (e) {
+        console.error(xhr.statusText);
+    };
+    xhr.send(null);
+   
     const { name = 'World' } = req.query;
     res.send(`Hello ${name}!`);
 };
