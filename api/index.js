12673fde7dee4e8fca593634425db66c642f6740
yulei <1442268726@qq.com>
2021-01-06 18:45:16 +0800

    ces

--- api/index.js
+++ api/index.js
@@ -1,22 +1,4 @@
 module.exports = (req, res) => {
-    //     var xhr = new XMLHttpRequest();
-    //     xhr.open('GET', 'http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=计算', true);
-    //     xhr.onload = function (e) {
-    //         if (xhr.readyState === 4) {
-    //             if (xhr.status === 200) {
-    //                 console.log(xhr.responseText);
-    //             } else {
-    //                 console.error(xhr.statusText);
-    //             }
-    //         }
-    //     };
-    //     xhr.onerror = function (e) {
-    //         console.error(xhr.statusText);
-    //     };
-    //     xhr.send(null);
-    var xhr = new XMLHttpRequest();
-    console.log(xhr);
-
     const { name = 'World' } = req.query;
     res.send(`Hello ${name}!`);
 };
