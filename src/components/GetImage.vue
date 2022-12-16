4a70c184f1b4b1e7eafbcc29f28da4983d4372fe
zhangyulei <zhangyulei@haocang.com>
2022-12-16 17:14:29 +0800

    xg

--- src/components/GetImage.vue
+++ src/components/GetImage.vue
@@ -1,122 +1,136 @@
 <template>
-  <div class="box">
-    <div>
-      <el-input
-        v-model="form.name"
-        v-on:keyup.enter="onSubmit"
-        style="width:300px;margin-right:20px"
-      ></el-input>
+    <div class="box">
+        <div>
+            <el-input
+                v-model="form.name"
+                v-on:keyup.enter="onSubmit"
+                style="width: 300px; margin-right: 20px"
+            ></el-input>
 
-      <el-button type="primary" @click="onSubmit">搜索</el-button>
-      <el-button @click="page++">下一页</el-button>
-    </div>
+            <el-button type="primary" @click="onSubmit">搜索</el-button>
+            <el-button @click="page++">下一页</el-button>
+        </div>
 
-    <div class="demo-image">
-      <div class="block" v-for="url in urls" :key="url.id">
-        <el-image class="item" :src="url.url_image" fit="contain"></el-image>
-      </div>
+        <div class="demo-image">
+            <div class="block" v-for="url in urls" :key="url.id">
+                <el-image
+                    class="item"
+                    :src="url.url_image"
+                    fit="contain"
+                ></el-image>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      form: {
-        name: ''
-      },
+    data() {
+        return {
+            form: {
+                name: "",
+            },
 
-      urls: [],
-      page: 1,
-      url:
-        'https://wall.alphacoders.com/api2.0/get.php?auth=1a1e07617b922b49f1f1efb53cf1326f&method=search&&width=1920&height=1080'
-    };
-  },
-  created() {
-    this.getNewImage();
-  },
-  mounted() {
-    window.addEventListener('scroll', function() {
-      console.log(document.documentElement.scrollTop); //代表目前滚动条和最上方的距离多长
-      console.log(document.documentElement.scrollHeight); //代表整个滚动条多长
-      console.log(document.documentElement.clientHeight);
-      console.log(
-        document.documentElement.scrollHeight -
-          document.documentElement.scrollTop -
-          document.documentElement.clientHeight
-      );
-    });
-  },
-  watch: {
-    page() {
-      if (this.form.name == '') {
+            urls: [],
+            page: 1,
+            url: "https://wall.alphacoders.com/api2.0/get.php?auth=1a1e07617b922b49f1f1efb53cf1326f&method=search&&width=1920&height=1080",
+        };
+    },
+    created() {
         this.getNewImage();
-      } else {
-        this.onSubmit();
-      }
-    }
-  },
+    },
+    mounted() {
+        window.addEventListener("scroll", function () {
+            console.log(document.documentElement.scrollTop); //代表目前滚动条和最上方的距离多长
+            console.log(document.documentElement.scrollHeight); //代表整个滚动条多长
+            console.log(document.documentElement.clientHeight);
+            console.log(
+                document.documentElement.scrollHeight -
+                    document.documentElement.scrollTop -
+                    document.documentElement.clientHeight
+            );
+        });
 
-  methods: {
-    async onSubmit() {
-      //fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=%E8%AE%A1%E7%AE%97
-
-      let form = {
-        ...this.form
-      };
-      console.log(form);
-      var reg = new RegExp('[\\u4E00-\\u9FFF]+', 'g');
-      let name;
-      if (reg.test(form.name)) {
-        let translatename = await this.axios.get(
-          `/youdao/translate?&doctype=json&type=AUTO&i=${form.name}`
-        );
-        name = translatename.data.translateResult[0][0].tgt;
-      } else {
-        name = form.name;
-      }
-
-      let url = `https://wall.alphacoders.com/api2.0/get.php?auth=1a1e07617b922b49f1f1efb53cf1326f&method=search&term=${name}&width=1920&height=1080&page=${this.page}`;
-
-      let { data } = await this.axios.get(url, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-
-      this.urls = data.wallpapers;
+        fetch(
+            "https://wall.alphacoders.com/search.php?search=%E4%BA%94%E6%98%9F%E7%BA%A2%E6%97%97"
+        )
+            .then((result) => {
+                console.log(result);
+                console.log(result.json());
+            })
+            .catch((eee) => {
+                console.log(eee);
+            });
+    },
+    watch: {
+        page() {
+            if (this.form.name == "") {
+                this.getNewImage();
+            } else {
+                this.onSubmit();
+            }
+        },
     },
 
-    getNewImage() {
-      let url = `https://wall.alphacoders.com/api2.0/get.php?auth=1a1e07617b922b49f1f1efb53cf1326f&method=highest_rated&page=10&info_level=2&page=${this.page}`;
-      this.axios
-        .get(url, {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        .then(res => {
-          console.log(res);
+    methods: {
+        async onSubmit() {
+            //fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=%E8%AE%A1%E7%AE%97
 
-          this.urls = res.data.wallpapers;
-        });
-    }
-  }
+            let form = {
+                ...this.form,
+            };
+            console.log(form);
+            var reg = new RegExp("[\\u4E00-\\u9FFF]+", "g");
+            let name;
+            if (reg.test(form.name)) {
+                let translatename = await this.axios.get(
+                    `/youdao/translate?&doctype=json&type=AUTO&i=${form.name}`
+                );
+                name = translatename.data.translateResult[0][0].tgt;
+            } else {
+                name = form.name;
+            }
+
+            let url = `https://wall.alphacoders.com/api2.0/get.php?auth=1a1e07617b922b49f1f1efb53cf1326f&method=search&term=${name}&width=1920&height=1080&page=${this.page}`;
+
+            let { data } = await this.axios.get(url, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            this.urls = data.wallpapers;
+        },
+
+        getNewImage() {
+            let url = `https://wall.alphacoders.com/api2.0/get.php?auth=1a1e07617b922b49f1f1efb53cf1326f&method=random&page=10&info_level=2&page=${this.page}`;
+            this.axios
+                .get(url, {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+                .then((res) => {
+                    console.log(res);
+
+                    this.urls = res.data.wallpapers;
+                });
+        },
+    },
 };
 </script>
 
 <style scoped>
 .demo-image {
-  margin: 10px;
-  column-count: 3;
-  column-gap: 10px;
+    margin: 10px;
+    column-count: 3;
+    column-gap: 10px;
 }
 .item {
-  margin-bottom: 10px;
+    margin-bottom: 10px;
 }
 .item img {
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 }
 </style>
